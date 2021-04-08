b521fd470830b76b9d2e4b4ca30eb6bdf3ca8ecf
何志刚 <zhiganghe1201@outlook.com>
2021-04-08 23:06:33 +0800

    feat: check node version

--- bin/main.js
+++ bin/main.js
@@ -1,17 +1,37 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const inquirer = require('inquirer');
 const chalk = require('chalk');
 const path = require('path');
-const didYouMean = require('didyoumean'); // 简易的智能匹配引擎
-const semver = require('semver'); // npm的语义版本包
-
+const didYouMean = require('didyoumean');
+const semver = require('semver');
 const updateCheck = require('../lib/update');
 
 didYouMean.threshold = 0.6;
 
-const { version } = require(path.resolve(__dirname, '../package.json'));
+const { version, engines } = require(path.resolve(__dirname, '../package.json'));
+
+function checkNodeVersion(wanted, cliName) {
+  // 检测node版本是否符合要求范围
+  if (!semver.satisfies(process.version, wanted)) {
+    console.log(
+      chalk.red(
+        'You are using Node ' +
+          process.version +
+          ', but this version of ' +
+          cliName +
+          ' requires Node ' +
+          wanted +
+          '.\nPlease upgrade your Node version.'
+      )
+    );
+    // 退出进程
+    process.exit(1);
+  }
+}
+
+// 检测node版本
+checkNodeVersion(engines.node, 'cci-gov');
 
 program.version(version, '-v, --version').usage('<command> [options]');
 
@@ -22,9 +42,7 @@ program
   .option('-d --dir <dir>', '创建目录')
   .action((name, cmd) => {
     validateArgsLen(process.argv.length, 4);
-    // const options = cleanArgs(cmd)
     require('../lib/creator')(name);
-    // console.log(name, cmd);
   });
 
 // 创建页面命令
@@ -33,17 +51,24 @@ program
   .description('create a new page')
   .option('-f, --force', 'Overwrite target directory if it exists')
   .action((name, cmd) => {
+    validateArgsLen(process.argv.length, 4);
     // TODO
   });
 
 // 删除命令
-program.command('delete <template-name>').description('delete a project template');
+program
+  .command('delete <template-name>')
+  .description('delete a project template')
+  .action((name, cmd) => {
+    // TODO
+  });
 
 // 更新命令
 program
   .command('upgrade')
   .description('Check the cci-gov version.')
   .action(() => {
+    validateArgsLen(process.argv.length, 3);
     updateCheck();
   });
 
@@ -55,25 +80,27 @@ program.arguments('<command>').action((cmd) => {
   suggestCommands(cmd);
 });
 
-program.parse(process.argv);
-
-// 输入easy显示帮助信息
+// 输入 cci-gov 显示帮助信息
 if (!process.argv.slice(2).length) {
   program.outputHelp();
 }
 
-// easy支持的命令
+program.parse(process.argv);
+
+// 处理命令拼写错误给出提示
 function suggestCommands(cmd) {
+  // cli 支持的命令
   const avaliableCommands = program.commands.map((cmd) => {
     return cmd._name;
   });
   // 简易智能匹配用户命令
   const suggestion = didYouMean(cmd, avaliableCommands);
   if (suggestion) {
-    console.log(`  ` + chalk.red(`Did you mean ${chalk.yellow(suggestion)}?`));
+    console.log(`  ` + chalk.red(`Did you mean ${chalk.yellow(suggestion)} ?`));
   }
 }
 
+// 对命令行字符超出参数规定给出提示
 function validateArgsLen(argvLen, maxArgvLens) {
   if (argvLen > maxArgvLens) {
     console.log(chalk.yellow('\n Info: You provided more than argument. the rest are ignored.'));
